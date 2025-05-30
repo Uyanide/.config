4cf84bfd8a869cb9f68eae08b28bfbee0311120f
Uyanide <pywang0608@foxmail.com>
2025-05-30 23:36:49 +0200

    fetch: change logo

--- fish/custom/ssh-agent.fish
+++ fish/custom/ssh-agent.fish
@@ -1,6 +1,6 @@
 if test -z "$SSH_AUTH_SOCK"
     eval (command ssh-agent -c) >/dev/null 2>&1
-    for key in $HOME/.ssh/github $HOME/.ssh/racknerd
+    for key in $HOME/.ssh/github $HOME/.ssh/racknerd $HOME/.ssh/penyanide $HOME/.ssh/tum
         if test -f $key
             command ssh-add $key >/dev/null 2>&1
         end
