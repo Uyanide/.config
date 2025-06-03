03f99c5740f5a403fe849b04f2546676ac1042f0
Uyanide <pywang0608@foxmail.com>
2025-06-03 05:02:28 +0200

    catppuccin🥰

--- fish/post.d/fetch.fish
+++ fish/post.d/fetch.fish
@@ -1,3 +1,4 @@
+# add 'set -g no_fetch' somewhere other than post.d to disable fetching
 if status is-interactive
     if not set -q no_fetch
         if type -q ff-brief
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,44 +1,31 @@
 return {
-  -- {
-  --   "catppuccin/nvim",
-  --   opts = {
-  --     flavor = "mocha",
-  --     transparent_background = true,
-  --   },
-  -- },
-  -- {
-  --   "navarasu/onedark.nvim",
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     require('onedark').setup {
-  --       style = 'darker',
-  --       transparent = true,
-  --     }
-  --     -- Enable theme
-  --     require('onedark').load()
-  --   end
-  -- },
   {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
-    config = function()
-      require("onedarkpro").setup({
-        options = {
-          bold = true,
-          italic = true,
-          underline = true,
-          cursorline = true,
-          transparency = true,
-        },
-        styles = {
-          comments = "italic",
-          keywords = "bold",
-          functions = "italic",
-        },
-      })
-      require("onedarkpro").load()
-    end,
+    "catppuccin/nvim",
+    opts = {
+      flavor = "macchiato",
+      transparent_background = true,
+      styles = {
+        functions = { "bold" },
+        keywords = { "bold" },
+      },
+    },
   },
+  -- {
+  --   "olimorris/onedarkpro.nvim",
+  --   priority = 1000, -- Ensure it loads first
+  --   config = function()
+  --     require("onedarkpro").setup({
+  --       options = {
+  --         transparency = true,
+  --       },
+  --       styles = {
+  --         comments = "italic",
+  --         keywords = "bold",
+  --       },
+  --     })
+  --     require("onedarkpro").load()
+  --   end,
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
