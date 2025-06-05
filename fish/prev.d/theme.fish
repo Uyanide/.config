6120b1f9e12a8c57eace17e8f457ff007469c01d
Uyanide <pywang0608@foxmail.com>
2025-06-06 01:22:55 +0200

    fish: save instead of set

--- fish/prev.d/theme.fish
+++ fish/prev.d/theme.fish
@@ -3,5 +3,5 @@ set -l theme 'Catpuccin Mocha'
 
 if not set -q fish_current_theme; or not string match -q "$theme" "$fish_current_theme"
     set -U fish_current_theme "$theme"
-    fish_config theme set "$theme"
+    fish_config theme save "$theme"
 end
