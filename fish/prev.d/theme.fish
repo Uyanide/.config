10d3d748786c9d18df5c13503e23379037428c71
Uyanide <pywang0608@foxmail.com>
2025-06-06 01:14:55 +0200

    fish: set theme automatically

---
+++ fish/prev.d/theme.fish
@@ -0,0 +1,7 @@
+set -l theme 'Catpuccin Mocha'
+
+
+if not set -q fish_current_theme; or not string match -q "$theme" "$fish_current_theme"
+    set -U fish_current_theme "$theme"
+    fish_config theme set "$theme"
+end
